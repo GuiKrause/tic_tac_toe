2cd0538e16220ebc8959e7a49ea1678a752f01f7
GuiKrause <guilhermekrauseramos@gmail.com>
2025-03-24 18:07:50 -0400

    feat: creating a filled board

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("!!! Welcome to the Tic Tac Toe game !!!");
+        char[][] board = new char[3][3];
+
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board.length; x++) {
+                board[y][x] = '#';
+            }
+        }
+
+        for(char[] i : board) {
+            System.out.println(i);
+        }
+    }
 }
