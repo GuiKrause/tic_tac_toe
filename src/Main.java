4cd5bdb767e894015a02afc0c0f8c228084168ee
Guilherme Krause <guilhermekrauseramos@gmail.com>
2025-03-24 20:34:11 -0400

    creating a tic tac toe board

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("!!! Welcome to the Tic Tac Toe game !!!");
-        char[][] board = new char[3][3];
 
-        for (int y = 0; y < board.length; y++) {
-            for (int x = 0; x < board.length; x++) {
-                board[y][x] = '#';
-            }
-        }
-
-        for(char[] i : board) {
-            System.out.println(i);
-        }
+        System.out.print("""
+                 X | X | X
+                ---+---+---
+                 X | X | X
+                ---+---+---
+                 X | X | X
+        """);
     }
 }
